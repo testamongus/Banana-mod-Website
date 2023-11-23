183f95de431d917875b3c406fea2f3c573ebdf69
nmsderp <130254323+nmsderp@users.noreply.github.com>
2023-11-23 13:31:13 -0500

    flag

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -22,6 +22,7 @@
     <h1>Whoops!</h1>
     <p>
         Something's not quite right. Maybe you should <a href="../">go back?</a>
+        <a href="https://info.flagcounter.com/c39u"><img src="https://s01.flagcounter.com/count2/c39u/bg_FFFFFF/txt_000000/border_CCCCCC/columns_8/maxflags_250/viewers_0/labels_1/pageviews_0/flags_0/percent_0/" alt="Flag Counter" border="0"></a>
     </p>
 
     <p>Error: {$page.status} - {$page.error.message}</p>

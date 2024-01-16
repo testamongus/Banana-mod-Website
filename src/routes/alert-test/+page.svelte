60f7e7d372795ba31343e99084418f5a314e91cd
nmsderp <130254323+nmsderp@users.noreply.github.com>
2024-01-15 20:26:56 -0500

    make alert test public cuz why not

---
+++ src/routes/alert-test/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Swal from 'sweetalert2';
+    
+    function showAlert() {
+      Swal.fire({
+                      title: 'Project Succesfuly Uploaded!',
+                      text: "Click the button below to view your project.",
+                      confirmButtonText: "Take me to my project",
+                      icon: "success",
+                      allowOutsideClick: false,
+                      preConfirm: () => {
+                          window.location.href = 'https://example.com';
+                      }
+                  })
+    }
+  </script>
+  
+  <button on:click={showAlert}>Click me</button>
+  

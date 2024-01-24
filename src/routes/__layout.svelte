25d699d712946ab4d53cc8a6f4356657707d8f42
nmsderp <130254323+nmsderp@users.noreply.github.com>
2024-01-24 17:50:49 -0500

    Update __layout.svelte

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,7 +3,7 @@
       // Check if the current domain is snail-ide.vercel.app
       if (window.location.hostname === 'snail-ide.vercel.app') {
         // Redirect to snail-ide.com
-        window.location.href = 'https://snail-ide.com' + window.location.pathname + window.location.search;
+        window.location.href = "https://snail-ide.com" + window.location.pathname + window.location.search;
       }
     }
   </script>
